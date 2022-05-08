0e1f1405d1095a6cc0acf49df11fcf804e470472
jacoby149 <jackstudent149@gmail.com>
2022-05-08 09:30:35 -0400

    ignored node modules and fixed bugs

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./index.css";
 import Container from "./components/Container/Container";
 import UserOptions from "./components/UserOptions/UserOptions";
 import Character from "./components/Character/Character";
+import BallColor from "./components/BallColor/BallColor";
 
 function App() {
   return (
