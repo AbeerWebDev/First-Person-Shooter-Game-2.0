3b6fcff26d31c4476f9de754ac8721db3e34fcf3
jacoby149 <jackstudent149@gmail.com>
2022-05-09 09:59:20 -0400

    got it to work with a regular img src

--- src/App.js
+++ src/App.js
@@ -8,17 +8,15 @@ import Container from "./components/Container/Container";
 import UserOptions from "./components/UserOptions/UserOptions";
 import Character from "./components/Character/Character";
 import BallColor from "./components/BallColor/BallColor";
-<<<<<<< HEAD
-import { ReactComponent as CogIcon } from './icons/cog.svg';
-=======
->>>>>>> ee765fbd48c1af1b8b9a950d3242691b755c94f9
+import CogIcon from './icons/cog.svg';
 
 function App() {
   return (
     <div className="parent">
       <Navbar>
         {/* the navItem is the cog.svg which is a drop dowm menu for the settings */}
-        <NavItem icons={<CogIcon />}></NavItem>
+        <img src={CogIcon} alt="React Logo" />
+        {/* <NavItem icons={<CogIcon />}></NavItem> */}
       </Navbar>
      
       <Container/>
