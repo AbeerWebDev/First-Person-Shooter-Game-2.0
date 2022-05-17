c1196afce79a3b8914432bc1c1a6d9a29cc5572d
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-05-17 19:01:45 +0400

    Revert "fixed app import"
    This reverts commit 039a7644c170fb8517995470d442941131b5ba5f.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import * as React from 'react';
-import BasicModal from './components/basicModal/basicModal';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  zIndex:5
+};
+
+const cogStyle = {
+  width: 50,
+  height: 50,
+  zIndex:5
+}
+
 
 function App() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-
-        <BasicModal />
-
+    <div>
+      
+     <Button style={{position:'absolute',top:0,right:20,zIndex:7}} onClick={handleOpen}>
+         <CogIcon style={cogStyle}/>
+        </Button><Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Character Settings
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-
+import './index.css'
 
 import App from './App';
 
@@ -9,6 +9,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
     <App />
+ 
+ 
 );
 
