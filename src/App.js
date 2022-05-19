060010bb27a5d69860083b9c50254ec47ba3a2e5
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-05-19 21:17:39 +0400

    stepper and gear done - no styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,78 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
+import * as React from "react";
 
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import SettingsOutlinedIcon from "@material-ui/icons/SettingsOutlined";
+
+import { useTheme } from "@mui/material/styles";
+import MobileStepper from "@mui/material/MobileStepper";
+
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  backgroundColor: 'black',
-  border: '2px solid #000',
+  backgroundColor: "black",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-  zIndex:7
+  zIndex: 7,
 };
 
 const cogStyle = {
   width: 50,
   height: 50,
-  zIndex:5
-}
+  zIndex: 5,
+};
 
+function DotsMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  return (
+    <MobileStepper
+      variant="dots"
+      steps={6}
+      position="static"
+      activeStep={activeStep}
+      sx={{ maxWidth: 400, flexGrow: 1 }}
+      nextButton={
+        <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+          Next
+          {theme.direction === 'rtl' ? (
+            <KeyboardArrowLeft />
+          ) : (
+            <KeyboardArrowRight />
+          )}
+        </Button>
+      }
+      backButton={
+        <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+          {theme.direction === 'rtl' ? (
+            <KeyboardArrowRight />
+          ) : (
+            <KeyboardArrowLeft />
+          )}
+          Back
+        </Button>
+      }
+    />
+  );
+}
 
 function App() {
   const [open, setOpen] = React.useState(false);
@@ -33,61 +81,28 @@ function App() {
 
   return (
     <div>
-      
-     <Button style={{position:'absolute',top:0,right:20,zIndex:7}} onClick={handleOpen}>
-         <SettingsOutlinedIcon style={cogStyle}/>
-        </Button><Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Chuwhekfhawpiou
-            </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              uhkhkhkhkjhkhkhkhkhkhkhjkhkhkkhkhk
-            </Typography>
-          </Box>
-        </Modal>
+      <Button
+        style={{ position: "absolute", top: 0, right: 20, zIndex: 7 }}
+        onClick={handleOpen}
+      >
+        <SettingsOutlinedIcon style={cogStyle} />
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            hello
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            hello
+          </Typography>
+          <DotsMobileStepper />
+        </Box>
+      </Modal>
     </div>
   );
 }
 
-
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-// // import Cog from '../../icons/cog.png'
-
-
-// function NavItem(props) {
-    
-//     const [open, setOpen] = useState(false)
-    
-//     return(
-       
-//        <li className="nav-item">
-//             <button className=".icon-button" onClick={() => setOpen(!open)}>{props.icon} </button>
-               
-    
-//             {open && props.children}
-//         </li>
-//     )
-// }
-
-// export default NavItem;
